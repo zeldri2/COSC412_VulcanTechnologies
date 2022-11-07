daf98b3e4912122c6956cf735eb4482b2e14bf24
zeldri2 <89756774+zeldri2@users.noreply.github.com>
2022-11-07 08:16:15 -0500

    Create index.php

---
+++ InventoryForge/index.php
@@ -0,0 +1,21 @@
+<?php include "templates/header.php"; ?>
+
+<!-- Navbar -->
+<center><img src="images/logo.png" alt="Logo Here" style="max-height: 80%; margin-left: 1%; margin-top: 1px"></center>
+  <div class="topnav">
+  <a class="active" href="../lookup.php">Inventory Lookup</a>
+  <a href="../update.php">Update Items</a>
+  <a href="../increase.php">Batch Increase</a>
+  <a href="../decrease.php">Batch Decrease</a>
+  <a href="../remove.php">Remove Items</a>
+</div>
+
+
+<div class="container">
+        <div class="alert_fail"> 
+            <h1>Inventory Forge Management System</h1>
+            <p>Description goes here</p>
+            <p>Instructions for page goes here</p>
+        </div>
+        </div>
+<?php include "templates/footer.php"; ?>
