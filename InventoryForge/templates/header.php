b8caf070fceb99ad398c871c65a42876c6ca81fc
zeldri2 <89756774+zeldri2@users.noreply.github.com>
2022-11-07 08:19:01 -0500

    Create header.php

---
+++ InventoryForge/templates/header.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en"> <!-- Declare document page as using the english language -->
+  <head>
+    <meta charset="utf-8" /> <!-- declare characterset UTF-8 which is how your typed characters are converted into machine-readable code --> 
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>PFChangs</title>
+    <link rel="stylesheet" href="style.css" /> 
+    <style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.topnav {
+  overflow: hidden;
+  background-color: #bfb013;
+}
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+.topnav a.active {
+  background-color: #877300;
+  color: white;
+}
+* {
+  box-sizing: border-box;
+}
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+input[type=submit] {
+  background-color: #877300;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+input[type=submit]:hover {
+  background-color: #ddd;
+}
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+html {
+	height: 100%;
+	/*Image only BG fallback*/
+	
+	/*background = gradient + image pattern combo*/
+	/*background: linear-gradient(rgba(196, 102, 0, 0.6), rgba(155, 89, 182, 0.6));*/
+}
+.alert_fail {
+    color: #721c24;
+    /*
+    background-color: #f8d7da;
+    border-color: #f5c6cb; */
+    position: relative;
+    padding: .75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-top-color: transparent;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+    border-left-color: transparent;
+    border-radius: .25rem;
+}
+.container {
+    display: flex;
+    justify-content: center;
+    width: 100;
+    padding: 70px 0px 600px 0px;
+  }
+  table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+td,
+th {
+  padding: 5px;
+  border-bottom: 1px solid #aaa;
+}
+</style>
+  </head>
+  <body>
+   <!-- <h1>InventoryForge</h1> -->
+  </body>
+</html>
