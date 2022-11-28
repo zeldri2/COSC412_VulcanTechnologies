d6f00de6157ac8d3c282383033170ae118aaebb5
zeldri2 <89756774+zeldri2@users.noreply.github.com>
2022-11-28 10:18:48 -0500

    Add files via upload
    Finished increase and decrease

--- InventoryForge/decrease.php
+++ InventoryForge/decrease.php
@@ -5,58 +5,59 @@
 require "config.php";
 require "common.php";
 
-if (isset($_POST['sumbit'])) {
+if (isset($_POST['submit'])) {
+	try{
+		$connection = new PDO($dsn, $username, $password, $options);
 
-try{
-	$connection = new PDO($dsn, $username, $password, $options);
+		$sql1 = $connection->prepare("UPDATE items SET quantity = quantity - :quantity1 WHERE itemID = :itemID1");
+		$sql1->bindParam(':quantity1', $quantity1, PDO::PARAM_STR);
+		$sql1->bindParam(':itemID1', $itemID1, PDO::PARAM_STR);
+		$quantity1 = $_POST['quantity1'];
+		$itemID1 = $_POST['itemID1'];
+		$sql1->execute();
 
-	$client= array(
-    	"itemID1" => $_POST['itemID1'],
-    	"quantity1" => $_POST['quantity1'],
-    	"itemID2" => $_POST['itemID2'],
-    	"quantity2" => $_POST['quantity2'],
-    	"itemID3" => $_POST['itemID3'],
-    	"quantity3" => $_POST['quantity3'],
-    	"itemID4" => $_POST['itemID4'],
-    	"quantity4" => $_POST['quantity4'],
-    	"itemID5" => $_POST['itemID5'],
-    	"quantity5" => $_POST['quantity5'],
-    	
-    	);
+		$sql2 = $connection->prepare("UPDATE items SET quantity = quantity - :quantity2 WHERE itemID = :itemID2");
+		$sql2->bindParam(':quantity2', $quantity2, PDO::PARAM_STR);
+		$sql2->bindParam(':itemID2', $itemID2, PDO::PARAM_STR);
+		$quantity2 = $_POST['quantity2'];
+		$itemID2 = $_POST['itemID2'];
+		$sql2->execute();
 
-	$sql = "UPDATE items SET quantity = quantity - quantity1 WHERE itemID = itemID1"; "UPDATE items SET quantity = quantity - quantity2 WHERE itemID = itemID2"; "UPDATE items SET quantity = quantity - quantity3 WHERE itemID = itemID3"; "UPDATE items SET quantity = quantity - quantity4 WHERE itemID = itemID4"; "UPDATE items SET quantity = quantity - quantity5 WHERE itemID = itemID5";
+		$sql3 = $connection->prepare("UPDATE items SET quantity = quantity - :quantity3 WHERE itemID = :itemID3");
+		$sql3->bindParam(':quantity3', $quantity3, PDO::PARAM_STR);
+		$sql3->bindParam(':itemID3', $itemID3, PDO::PARAM_STR);
+		$quantity3 = $_POST['quantity3'];
+		$itemID3 = $_POST['itemID3'];
+		$sql3->execute();
 
-	$statement = $connection->prepare($sql);
-	$statement->bindParam('quanity1', $quantity,PDO::PARAM_STR);
-	$statement->bindParam('itemID1', $itemID,PDO::PARAM_STR);
-	$statement->bindParam('quanity2', $quantity,PDO::PARAM_STR);
-	$statement->bindParam('itemID2', $itemID,PDO::PARAM_STR);
-	$statement->bindParam('quanity3', $quantity,PDO::PARAM_STR);
-	$statement->bindParam('itemID3', $itemID,PDO::PARAM_STR);
-	$statement->bindParam('quanity4', $quantity,PDO::PARAM_STR);
-	$statement->bindParam('itemID4', $itemID,PDO::PARAM_STR);
-	$statement->bindParam('quanity5', $quantity,PDO::PARAM_STR);
-	$statement->bindParam('itemID5', $itemID,PDO::PARAM_STR);
+		$sql4 = $connection->prepare("UPDATE items SET quantity = quantity - :quantity4 WHERE itemID = :itemID4");
+		$sql4->bindParam(':quantity4', $quantity4, PDO::PARAM_STR);
+		$sql4->bindParam(':itemID4', $itemID4, PDO::PARAM_STR);
+		$quantity4 = $_POST['quantity4'];
+		$itemID4 = $_POST['itemID4'];
+		$sql4->execute();
 
-	$statement->execute($client);	
-
-	$success = "items updated";
- }
+		$sql5 = $connection->prepare("UPDATE items SET quantity = quantity - :quantity5 WHERE itemID = :itemID5");
+		$sql5->bindParam(':quantity5', $quantity5, PDO::PARAM_STR);
+		$sql5->bindParam(':itemID5', $itemID5, PDO::PARAM_STR);
+		$quantity5 = $_POST['quantity5'];
+		$itemID5 = $_POST['itemID5'];
+		$sql5->execute();
+	}
  	catch(PDOException $error) {
-    echo $sql . "<br>" . $error->getMessage();
-  }
- }
-
- ?>
+    echo $error->getMessage();
+  	}
+}
+?>
  
- <?php require "templates/header.php"; ?>
+<?php require "templates/header.php"; ?>
  
  <h2>Decrease at once</h2>
  
- 	<h4> enter multiple itemID to decrease multiple quantitys at once</h4>
+ 	<h4> Enter multiple itemID to decrease multiple quantities at once</h4>
  	
  	<form method="post">
- 	 <label for="itemID1">itemID 1</label><br>
+ 	 		<label for="itemID1">itemID 1</label><br>
       	 <input type="text" name="itemID1" id="itemID1"><br>
       	 <label for="quantity1">quantity to decrease</label><br>
       	 <input type="text" name="quantity1" id="quantity1"><br>
@@ -76,7 +77,7 @@ try{
       	 <label for="quantity4">quantity to decrease</label><br>
       	 <input type="text" name="quantity4" id="quantity4"><br>
       	 
-      	 <label for="itemID5">item5</label><br>
+      	 <label for="itemID5">itemID 5</label><br>
       	 <input type="text" name="itemID5" id="itemID5"><br>
       	 <label for="quantity5">quantity to decrease</label><br>
       	 <input type="text" name="quantity5" id="quantity5"><br>
@@ -87,5 +88,3 @@ try{
  
 
 <a href="index.php">Back to home</a>
-
-<?php require "templates/footer.php"; ?>
--- InventoryForge/increase.php
+++ InventoryForge/increase.php
@@ -5,43 +5,49 @@
 require "config.php";
 require "common.php";
 
-if (isset($_POST['sumbit'])) {
+if (isset($_POST['submit'])) {
 	try{
 		$connection = new PDO($dsn, $username, $password, $options);
 
-		$client= array(
-	    	"itemID1" => $_POST['itemID1'],
-	    	"quantity1" => $_POST['quantity1'],
-	    	"itemID2" => $_POST['itemID2'],
-	    	"quantity2" => $_POST['quantity2'],
-	    	"itemID3" => $_POST['itemID3'],
-	    	"quantity3" => $_POST['quantity3'],
-	    	"itemID4" => $_POST['itemID4'],
-	    	"quantity4" => $_POST['quantity4'],
-	    	"itemID5" => $_POST['itemID5'],
-	    	"quantity5" => $_POST['quantity5']
-	    );
+		$sql1 = $connection->prepare("UPDATE items SET quantity = quantity + :quantity1 WHERE itemID = :itemID1");
+		$sql1->bindParam(':quantity1', $quantity1, PDO::PARAM_STR);
+		$sql1->bindParam(':itemID1', $itemID1, PDO::PARAM_STR);
+		$quantity1 = $_POST['quantity1'];
+		$itemID1 = $_POST['itemID1'];
+		$sql1->execute();
 
-		$sql = "UPDATE items SET quantity = quantity + :quantity1 WHERE itemID = :itemID1";"UPDATE items SET quantity = quantity + :quantity2 WHERE itemID = :itemID2";"UPDATE items SET quantity = quantity + :quantity3 WHERE itemID = :itemID3";"UPDATE items SET quantity = quantity + :quantity4 WHERE itemID = :itemID4";"UPDATE items SET quantity = quantity + :quantity5 WHERE itemID = :itemID5";
+		$sql2 = $connection->prepare("UPDATE items SET quantity = quantity + :quantity2 WHERE itemID = :itemID2");
+		$sql2->bindParam(':quantity2', $quantity2, PDO::PARAM_STR);
+		$sql2->bindParam(':itemID2', $itemID2, PDO::PARAM_STR);
+		$quantity2 = $_POST['quantity2'];
+		$itemID2 = $_POST['itemID2'];
+		$sql2->execute();
 
-		$statement = $connection->prepare($sql);
-		$statement->bindParam('quanity1', $quantity1, PDO::PARAM_INT);
-		$statement->bindParam('itemID1', $itemID1, PDO::PARAM_INT);
-		$statement->bindParam('quanity2', $quantity2, PDO::PARAM_INT);
-		$statement->bindParam('itemID2', $itemID2, PDO::PARAM_INT);
-		$statement->bindParam('quanity3', $quantity3, PDO::PARAM_INT);
-		$statement->bindParam('itemID3', $itemID3, PDO::PARAM_INT);
-		$statement->bindParam('quanity4', $quantity4, PDO::PARAM_INT);
-		$statement->bindParam('itemID4', $itemID4, PDO::PARAM_INT);
-		$statement->bindParam('quanity5', $quantity5, PDO::PARAM_INT);
-		$statement->bindParam('itemID5', $itemID5, PDO::PARAM_INT);
-		$statement->execute($client);	
+		$sql3 = $connection->prepare("UPDATE items SET quantity = quantity + :quantity3 WHERE itemID = :itemID3");
+		$sql3->bindParam(':quantity3', $quantity3, PDO::PARAM_STR);
+		$sql3->bindParam(':itemID3', $itemID3, PDO::PARAM_STR);
+		$quantity3 = $_POST['quantity3'];
+		$itemID3 = $_POST['itemID3'];
+		$sql3->execute();
+
+		$sql4 = $connection->prepare("UPDATE items SET quantity = quantity + :quantity4 WHERE itemID = :itemID4");
+		$sql4->bindParam(':quantity4', $quantity4, PDO::PARAM_STR);
+		$sql4->bindParam(':itemID4', $itemID4, PDO::PARAM_STR);
+		$quantity4 = $_POST['quantity4'];
+		$itemID4 = $_POST['itemID4'];
+		$sql4->execute();
+
+		$sql5 = $connection->prepare("UPDATE items SET quantity = quantity + :quantity5 WHERE itemID = :itemID5");
+		$sql5->bindParam(':quantity5', $quantity5, PDO::PARAM_STR);
+		$sql5->bindParam(':itemID5', $itemID5, PDO::PARAM_STR);
+		$quantity5 = $_POST['quantity5'];
+		$itemID5 = $_POST['itemID5'];
+		$sql5->execute();
 	}
  	catch(PDOException $error) {
-    echo $sql . "<br>" . $error->getMessage();
+    echo $error->getMessage();
   	}
 }
-
 ?>
  
 <?php require "templates/header.php"; ?>
@@ -82,5 +88,3 @@ if (isset($_POST['sumbit'])) {
  
 
 <a href="index.php">Back to home</a>
-
-<?php require "templates/footer.php"; ?>
